6f935586a4d4080222eb83136529fac543f1f93b
Alvaro Denis <denisacostaq@gmail.com>
2013-11-20 00:53:11 -0500

    new file:   AUTHORS 	new file:   COPYING 	new file:   ChangeLog 	new file:   HACKING 	new file:   INSTALL 	new file:   MAINTAINERS 	new file:   Makefile.am 	new file:   NEWS 	modified:   README 	new file:   THANKS 	new file:   TODO 	new file:   configure.ac 	new file:   headers/Makefile.am 	new file:   headers/matX.h 	new file:   lib/Makefile.am 	new file:   lib/libmatX.pc.in 	new file:   lib/matX.cc This is an example of the use of libtoolo

---
+++ headers/matX.h
@@ -0,0 +1,13 @@
+namespace matematicsX
+{
+  class matX
+  {
+  public:
+    matX ();
+    matX (const matX& orig);
+    ~matX ();
+
+    int plusXY (int x, int y);
+    int multXY (int x, int y);
+  };
+};
---
+++ lib/matX.cc
@@ -0,0 +1,25 @@
+#include <matX.h>
+namespace matematicsX
+{
+	matX::matX ()
+	{		
+	}
+	
+	matX::matX (const matX& orig)
+	{
+	}
+
+	matX::~matX ()
+	{		
+	}
+	
+	int matX::plusXY (int x, int y)
+	{
+		return x + y;
+	}
+  
+	int matX::multXY (int x, int y)
+	{
+		return x * y;
+	}
+}
